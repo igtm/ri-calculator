4bb1e28d4b0ad8873e7b79a410c4bceebc04b247
Tomokatsu Iguchi <tomo14916253@gmail.com>
2022-11-04 23:57:20 +0900

    [add] tab

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,21 @@
-mod terminal;
 mod model_ec2;
+mod terminal;
 
 use aws_sdk_ec2 as ec2;
-use aws_smithy_types;
-// use cli_table::{format::Justify, print_stdout, Cell, Style, Table, CellStruct, WithTitle};
-use ec2::{Client, model::{ReservedInstanceState, InstanceStateName, Instance, ReservedInstances}, output::{DescribeReservedInstancesOutput, DescribeInstancesOutput}};
 use crossterm::{
-  event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
-  execute,
-  terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    event::{DisableMouseCapture, EnableMouseCapture},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use ec2::{
+    output::{DescribeInstancesOutput, DescribeReservedInstancesOutput},
+    Client,
 };
 use std::{error::Error, io};
-use tui::{
-  backend::{Backend, CrosstermBackend},
-  layout::{Constraint, Layout},
-  style::{Color, Modifier, Style},
-  widgets::{Block, Borders, Cell, Row, Table, TableState},
-  Frame, Terminal, text::Spans,
-};
+use tui::{backend::CrosstermBackend, text::Spans, Terminal};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>>  {
+async fn main() -> Result<(), Box<dyn Error>> {
     let config = aws_config::load_from_env().await;
     let client = ec2::Client::new(&config);
 
@@ -30,16 +25,14 @@ async fn main() -> Result<(), Box<dyn Error>>  {
     // calc
     let mut out: model_ec2::EC2Instances = model_ec2::EC2Instances::new();
     for ri in instances.reservations().unwrap().into_iter() {
-      for instance in ri.instances().unwrap() {
-        out.push_from_instance(&instance);
-      }
+        for instance in ri.instances().unwrap() {
+            out.push_from_instance(&instance);
+        }
     }
 
     for i in ri.reserved_instances().unwrap().into_iter() {
-      out.push_from_reserved_instance(i);
+        out.push_from_reserved_instance(i);
     }
-
-    // assert!(print_stdout(out.with_title()).is_ok());
 
     // setup terminal
     enable_raw_mode()?;
@@ -50,14 +43,8 @@ async fn main() -> Result<(), Box<dyn Error>>  {
 
     // create app and run it
     let app = terminal::App::new(
-      terminal::TopWindowOptions::new(
-        vec![
-          Spans::from(format!("{}", "aaa")),
-        ]
-      ),
-      terminal::TableOptions::new(
-        out,
-      )
+        terminal::TopWindowOptions::new(vec![Spans::from(format!("{}", "aaa"))]),
+        terminal::TableOptions::new(out),
     );
     let res = terminal::run_app(&mut terminal, app);
 
@@ -78,102 +65,15 @@ async fn main() -> Result<(), Box<dyn Error>>  {
 }
 
 async fn get_ec2_ri(client: &Client) -> DescribeReservedInstancesOutput {
-
     // RI
     let ret = client.describe_reserved_instances().send().await.unwrap();
-
-    // let mut vec = vec![];
-    // for ri in ret.reserved_instances().unwrap().into_iter() {
-    //   vec.push(vec![
-    //     get_cell(ri.availability_zone()),
-    //     get_cell(ri.duration().and_then(|x| Some(x / (60 * 60 * 24 * 365)))),
-    //     get_cell(ri.end().and_then(|x| x.fmt(aws_smithy_types::date_time::Format::DateTime).ok()).or_else(|| None)),
-    //     // get_cell(ri.fixed_price()),
-    //     get_cell(ri.instance_count()),
-    //     get_cell(ri.instance_type().map(|x| x.as_str())),
-    //     get_cell(ri.product_description().map(|x| x.as_str())),
-    //     // get_cell(ri.reserved_instances_id()),
-    //     get_cell(ri.start().and_then(|x| x.fmt(aws_smithy_types::date_time::Format::DateTime).ok()).or_else(|| None)),
-    //     get_cell(ri.state()),
-    //     // get_cell(ri.usage_price()),
-    //     // get_cell(ri.currency_code()),
-    //     get_cell(ri.instance_tenancy()),
-    //     get_cell(ri.offering_class()),
-    //     get_cell(ri.offering_type()),
-    //     // get_cell(ri.recurring_charges()),
-    //     get_cell(ri.scope()),
-    //   ])
-    // }
-
-    // // cli-table
-    // let table = vec.table()
-    // .title(vec![
-    //     "availability_zone".cell().bold(true),
-    //     "duration".cell().bold(true),
-    //     "end".cell().bold(true),
-    //     // "fixed_price".cell().bold(true),
-    //     "instance_count".cell().bold(true),
-    //     "instance_type".cell().bold(true),
-    //     "product_description".cell().bold(true),
-    //     // "reserved_instances_id".cell().bold(true),
-    //     "start".cell().bold(true),
-    //     "state".cell().bold(true),
-    //     // "usage_price".cell().bold(true),
-    //     // "currency_code".cell().bold(true),
-    //     "instance_tenancy".cell().bold(true),
-    //     "offering_class".cell().bold(true),
-    //     "offering_type".cell().bold(true),
-    //     // "recurring_charges".cell().bold(true),
-    //     "scope".cell().bold(true),
-    // ])
-    // .bold(true);
-
-    // assert!(print_stdout(table).is_ok());
 
     return ret;
 }
 
 async fn get_ec2_instances(client: &Client) -> DescribeInstancesOutput {
-
     // RI
     let ret = client.describe_instances().send().await.unwrap();
 
-    // let mut vec = vec![];
-    // for ri in ret.reservations().unwrap().into_iter() {
-    //   for instance in ri.instances().unwrap() {
-    //     vec.push(vec![
-    //       get_cell(instance.instance_type().map(|x| x.as_str())),
-    //       get_cell(instance.state().and_then(|x| x.name.as_ref())),
-    //       // get_cell(instance.platform()),
-    //       get_cell(instance.platform_details()),
-    //       get_cell(instance.placement().and_then(|x| x.availability_zone())),
-    //       get_cell(instance.placement().and_then(|x| x.tenancy())),
-    //     ])
-    //   }
-    // }
-
-    // // cli-table
-    // let table = vec
-    // .table()
-    // .title(vec![
-    //     "instance_type".cell().bold(true),
-    //     "state".cell().bold(true),
-    //     // "platform".cell().bold(true),
-    //     "platform_details".cell().bold(true),
-    //     "availability_zone".cell().bold(true),
-    //     "tenancy".cell().bold(true),
-    // ])
-    // .bold(true);
-
-    // assert!(print_stdout(table).is_ok());
-
     return ret;
 }
-
-// fn get_cell<T: std::fmt::Debug>(s: std::option::Option<T>) -> CellStruct {
-//   let str = match s {
-//     Some(x) => format!("{:?}", x),
-//     None => "".to_owned(),
-//   };
-//   return str.cell()
-// }
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,79 +1,105 @@
-use crossterm::{
-  event::{self, Event, KeyCode},
-};
-use std::{io};
+use crossterm::event::{self, Event, KeyCode};
+use std::io;
 use tui::{
-  backend::{Backend},
-  layout::{Constraint, Layout},
-  style::{Color, Modifier, Style},
-  widgets::{Block, Borders, Cell, Row, Table, TableState, Paragraph},
-  text::{Span, Spans},
-  Frame, Terminal,
+    backend::Backend,
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    text::{Span, Spans},
+    widgets::{Block, Borders, Cell, Paragraph, Row, Table, TableState, Tabs},
+    Frame, Terminal,
 };
 
 use crate::model_ec2::{EC2Instances, ViewMode};
 
 pub struct TopWindowOptions<'a> {
-  content: Vec<Spans<'a>>,
+    content: Vec<Spans<'a>>,
 }
 impl<'a> TopWindowOptions<'a> {
-  pub fn new(content: Vec<Spans<'a>>) -> TopWindowOptions<'a> {
-      TopWindowOptions {
-        content: content,
-      }
-  }
+    pub fn new(content: Vec<Spans<'a>>) -> TopWindowOptions<'a> {
+        TopWindowOptions { content: content }
+    }
 }
 pub struct TableOptions {
-  ec2_instances: EC2Instances
+    ec2_instances: EC2Instances,
 }
 impl TableOptions {
-  pub fn new(ec2_instances: EC2Instances) -> TableOptions {
-      TableOptions {
-        ec2_instances: ec2_instances,
-      }
-  }
+    pub fn new(ec2_instances: EC2Instances) -> TableOptions {
+        TableOptions {
+            ec2_instances: ec2_instances,
+        }
+    }
 }
 pub struct App<'a> {
-  state: TableState,
-  top_window_options: TopWindowOptions<'a>,
-  table_options: TableOptions
+    state: TableState,
+    top_window_options: TopWindowOptions<'a>,
+    table_options: TableOptions,
+    tab_index: usize,
+    tab_titles: Vec<&'a str>,
 }
 
 impl<'a> App<'a> {
-  pub fn new(top_window_options: TopWindowOptions<'a>, table_options: TableOptions) -> App<'a> {
-      App {
-          state: TableState::default(),
-          top_window_options: top_window_options,
-          table_options: table_options,
-      }
-  }
-  pub fn next(&mut self) {
-      let i = match self.state.selected() {
-          Some(i) => {
-              if i >= self.table_options.ec2_instances.to_vec().len() - 1 {
-                  0
-              } else {
-                  i + 1
-              }
-          }
-          None => 0,
-      };
-      self.state.select(Some(i));
-  }
+    pub fn new(top_window_options: TopWindowOptions<'a>, table_options: TableOptions) -> App<'a> {
+        App {
+            state: TableState::default(),
+            top_window_options: top_window_options,
+            table_options: table_options,
+            tab_index: 0,
+            tab_titles: vec!["Instance", "NormalizationFactor"],
+        }
+    }
+    pub fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.table_options.ec2_instances.to_vec().len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
 
-  pub fn previous(&mut self) {
-      let i = match self.state.selected() {
-          Some(i) => {
-              if i == 0 {
-                  self.table_options.ec2_instances.to_vec().len() - 1
-              } else {
-                  i - 1
-              }
-          }
-          None => 0,
-      };
-      self.state.select(Some(i));
-  }
+    pub fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.table_options.ec2_instances.to_vec().len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    pub fn next_tab(&mut self) {
+        self.tab_index = (self.tab_index + 1) % self.tab_titles.len();
+        self.set_view_mode();
+    }
+    pub fn previous_tab(&mut self) {
+        if self.tab_index > 0 {
+            self.tab_index -= 1;
+        } else {
+            self.tab_index = self.tab_titles.len() - 1;
+        }
+        self.set_view_mode();
+    }
+    fn set_view_mode(&mut self) {
+        match self.tab_index {
+            0 => self
+                .table_options
+                .ec2_instances
+                .set_view_mode(ViewMode::Instance),
+            1 => self
+                .table_options
+                .ec2_instances
+                .set_view_mode(ViewMode::NormalizationFactor),
+            _ => {}
+        }
+    }
 }
 
 pub fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
@@ -83,10 +109,12 @@ pub fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Resu
         if let Event::Key(key) = event::read()? {
             match key.code {
                 KeyCode::Char('q') => return Ok(()),
-                KeyCode::Char('d') => app.table_options.ec2_instances.set_view_mode(ViewMode::Instance),
-                KeyCode::Char('n') => app.table_options.ec2_instances.set_view_mode(ViewMode::NormalizationFactor),
+                // KeyCode::Char('d') => app.table_options.ec2_instances.set_view_mode(ViewMode::Instance),
+                // KeyCode::Char('n') => app.table_options.ec2_instances.set_view_mode(ViewMode::NormalizationFactor),
                 KeyCode::Down => app.next(),
                 KeyCode::Up => app.previous(),
+                KeyCode::Left => app.previous_tab(),
+                KeyCode::Right => app.next_tab(),
                 _ => {}
             }
         }
@@ -95,7 +123,8 @@ pub fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Resu
 
 pub fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
     let rects = Layout::default()
-        .constraints([Constraint::Percentage(20), Constraint::Percentage(80)].as_ref())
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Length(3), Constraint::Min(0)].as_ref())
         .margin(0)
         .split(f.size());
 
@@ -105,9 +134,7 @@ pub fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
     let header_cells = binding
         .iter()
         .map(|h| Cell::from(*h).style(Style::default().fg(Color::Red)));
-    let header = Row::new(header_cells)
-        .style(normal_style)
-        .height(1);
+    let header = Row::new(header_cells).style(normal_style).height(1);
     let binding = app.table_options.ec2_instances.to_vec();
     let rows = binding.iter().map(|item| {
         let height = item
@@ -122,17 +149,42 @@ pub fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
     });
     let t = Table::new(rows)
         .header(header)
-        .block(Block::default().borders(Borders::ALL).title(app.table_options.ec2_instances.title()))
+        .block(
+            Block::default()
+                .borders(Borders::ALL)
+                .title(app.table_options.ec2_instances.title()),
+        )
         .highlight_style(selected_style)
         .widths(app.table_options.ec2_instances.widths());
+    // Tab
+    let titles = app
+        .tab_titles
+        .iter()
+        .map(|t| {
+            let (first, rest) = t.split_at(1);
+            Spans::from(vec![
+                Span::styled(first, Style::default().fg(Color::Yellow)),
+                Span::styled(rest, Style::default().fg(Color::Green)),
+            ])
+        })
+        .collect();
+    let tabs = Tabs::new(titles)
+        .block(Block::default().borders(Borders::ALL).title("Tabs"))
+        .select(app.tab_index)
+        .style(Style::default().fg(Color::Cyan))
+        .highlight_style(
+            Style::default()
+                .add_modifier(Modifier::BOLD)
+                .bg(Color::Black),
+        );
+    f.render_widget(tabs, rects[0]);
     f.render_stateful_widget(t, rects[1], &mut app.state);
 
-
     // Block
-    let block = Block::default()
-      .borders(Borders::ALL)
-      .title("Summary");
-    let paragraph = Paragraph::new(app.top_window_options.content.clone())
-      .block(block);
-    f.render_widget(paragraph, rects[0]);
+    // let block = Block::default()
+    //   .borders(Borders::ALL)
+    //   .title("Summary");
+    // let paragraph = Paragraph::new(app.top_window_options.content.clone())
+    //   .block(block);
+    // f.render_widget(paragraph, rects[0]);
 }
